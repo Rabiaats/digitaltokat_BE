e0a95d3409fbe43afa989a3f284bf3a54db3fd82
Rabiaats <atesr782@gmail.com>
2025-05-06 14:02:40 +0300

    feat: permission middleware i eklendi, http i istelerini atanlari yetkilendirmek icin

---
+++ src/middlewares/permissions.js
@@ -0,0 +1,55 @@
+"use strict"
+/* -------------------------------------------------------
+    | FULLSTACK TEAM | NODEJS / EXPRESS |
+------------------------------------------------------- */
+// Middleware: permissions
+
+module.exports = {
+
+    isLogin: (req, res, next) => {
+
+        // Set Passive:
+        return next()
+
+        // any User:
+        if (req.user) {
+
+            next()
+
+        } else {
+
+            res.errorStatusCode = 403
+            throw new Error('Bu işlemi yapmak için giriş yapmanız gerekmektedir.')
+        }
+    },
+
+    isSuperAdmin: (req, res, next) => {
+
+        // Set Passive:
+        return next()
+        
+        // only Admin:
+        if (req.user && req.user.role == 'superadmin') {
+
+            next()
+
+        } else {
+
+            res.errorStatusCode = 403
+            throw new Error('Bu işlemi yapmak için izniniz bulunmamaktadır. Tokat Dijital olarak giriş yapmanız gerekmektedir.')
+        }
+    },
+
+    isFirmOrSuperAdmin: (req, res, next) => {
+
+        return next()
+
+
+        if(req.user && (req.user.role == 'firm' || req.user.role == 'superadmin')){
+            next();
+        }else{
+            res.errorStatusCode = 403;
+            throw new Error('Bu işlemi yapmak için izniniz bulunmamaktadır. Tokat Dijital ya da Fİrma Admin\'i olarak giriş yapmanız gerekmektedir.')
+        }
+    },
+}
