e2d80940196668b707bce0c97c8c26d5c012637a
Rabiaats <atesr782@gmail.com>
2025-05-13 14:10:18 +0300

    feat: gonderilen dosyalara ulasmak icin product ve firm route larina upload ekledim

--- src/routes/firm.js
+++ src/routes/firm.js
@@ -6,15 +6,16 @@ const router = require('express').Router()
 const {list, create, read, update, deletee} = require('../controllers/firm');
 
 const {isSuperAdmin, isFirmOrSuperAdmin} = require('../middlewares/permissions');
+const upload = require('../middlewares/upload');
 
 router.route('/')
     .get(list)
-    .post(isSuperAdmin,create)
+    .post(isSuperAdmin, upload.array('image'),create)
 
 router.route('/:id')
     .get(read)
-    .put(isFirmOrSuperAdmin, update)
-    .patch(isFirmOrSuperAdmin, update)
+    .put(isFirmOrSuperAdmin, upload.array('image'), update)
+    .patch(isFirmOrSuperAdmin, upload.array('image'), update)
     .delete(isFirmOrSuperAdmin, deletee)
 
 module.exports = router;
--- src/routes/product.js
+++ src/routes/product.js
@@ -6,15 +6,16 @@ const router = require('express').Router()
 const {list, create, read, update, deletee} = require('../controllers/product');
 
 const {isFirmOrSuperAdmin} = require('../middlewares/permissions');
+const upload = require('../middlewares/upload');
 
 router.get('/', list)
 router.get('/:id', read)
 
 router.use(isFirmOrSuperAdmin)
-router.post('/', create)
+router.post('/', upload.single('image'), create)
 router.route('/:id')
-    .put(update)
-    .patch(update)
+    .put(upload.single('image'), update)
+    .patch(upload.single('image'), update)
     .delete(deletee)
 
 module.exports = router;
